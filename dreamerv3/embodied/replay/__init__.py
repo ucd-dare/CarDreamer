4679d8b28dc69d75dd7f9160e95558b35c3e2181
Jung Dongwon <dongwonj@usc.edu>
2024-09-17 15:55:41 -0700

    Update __init__.py

--- dreamerv3/embodied/replay/__init__.py
+++ dreamerv3/embodied/replay/__init__.py
@@ -1,8 +1,8 @@
 from . import limiters, selectors
-from .adversarial import AdversarialReplay
-from .count_based import CountBasedReplay
-from .curious_replay import CuriousReplay
 from .generic import Generic
 from .prioritized_experience_replay import PrioritizedExperienceReplay
 from .replays import Uniform
 from .reverb import Reverb
+from .curious_replay import CuriousReplay
+from .adversarial import AdversarialReplay
+from .count_based import CountBasedReplay
