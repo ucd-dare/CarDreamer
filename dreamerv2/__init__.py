fa91aa0ffa7ac09f5f8c84aaeb6f8623781262a2
Dechen <gaodechen@live.cn>
2024-09-17 18:20:53 -0700

    Fix DMv2 circular imports due to pre-commit

--- dreamerv2/__init__.py
+++ dreamerv2/__init__.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     pass
 
-from . import embodied, envs, replay, run
 from .core import *
+from . import embodied, envs, replay, run
 
 __all__ = [k for k, v in list(locals().items()) if type(v).__name__ in ("type", "function") and not k.startswith("_")]
